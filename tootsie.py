2a82cd342bff5b27027f8063a586a84755f24829
piqey <jckusho@gmail.com>
2021-02-01 10:01:45 -0500

    Bot now prints its current guilds

--- tootsie.py
+++ tootsie.py
@@ -8,10 +8,18 @@ load_dotenv()
 # Grab the token from the environment file
 TOKEN = os.getenv("DISCORD_TOKEN")
 
+# Store an instance the Discord Client class
 client = discord.Client()
 
+# Attach to the client's events
 @client.event
 async def on_ready():
+    # Announce oneself
     print(f"yoooo {client.user} is in the house")
+
+    # Loop through all the guilds (servers) the bot is a part of
+    for guild in client.guilds:
+        # Print the current guild
+        print(f"{client.user} is connected to {guild.name} (ID: {guild.id})")
 
 client.run(TOKEN)
